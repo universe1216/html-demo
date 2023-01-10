1c362b5f6837a6f4f278d488f5fd9d551b872294
universe1216 <marcus727.dev@outlook.com>
2023-01-10 23:50:13 +0530

    Adding Digital Marketing Agency

---
+++ digital_marketing/assets/js/main.js
@@ -0,0 +1,71 @@
+$(function () {
+  "use strict";
+
+  //====== Magnific Popup
+
+  $(".video-popup").magnificPopup({
+    type: "iframe",
+    // other options
+  });
+
+  //===== Magnific Popup
+
+  $(".image-popup").magnificPopup({
+    type: "image",
+    gallery: {
+      enabled: true,
+    },
+  });
+
+  // Go to Top
+
+  // Scroll Event
+  $(window).on("scroll", function () {
+    var scrolled = $(window).scrollTop();
+    if (scrolled > 300) $(".go-top").addClass("active");
+    if (scrolled < 300) $(".go-top").removeClass("active");
+  });
+
+  // Click Event
+  $(".go-top").on("click", function () {
+    $("html, body").animate(
+      {
+        scrollTop: "0",
+      },
+      1200
+    );
+  });
+
+  //=====  WOW active
+
+  new WOW().init();
+
+  //=====
+});
+
+// scroll to top
+
+//Get the button
+var mybutton = document.getElementById("topBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (
+    document.body.scrollTop > 200 ||
+    document.documentElement.scrollTop > 200
+  ) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
