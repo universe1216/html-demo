710a5c130755ed5670fb7f0f131fb34cdda62d0d
universe1216 <marcus727.dev@outlook.com>
2023-01-22 17:14:30 +0530

    Seahomes Website Removing Assets

--- seahomes/assets/js/main.js
+++ seahomes/assets/js/main.js
@@ -1,6 +1,6 @@
 
-$(function() {
-  $(document).scroll(function() {
+$(function () {
+  $(document).scroll(function () {
     var $nav = $(".navbar-scroller");
     $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
   });
@@ -37,5 +37,62 @@ function topFunction() {
   document.documentElement.scrollTop = 0;
 }
 
-// Read More Read Less
+Splitting();
+AOS.init();
+function navFunction(x) {
+  x.classList.toggle("change");
+}
+const btn = document.getElementById("btn");
+const nav = document.getElementById("nav");
+btn.addEventListener("click", () => {
+  nav.classList.toggle("active");
+  btn.classList.toggle("active");
+});
+document.getElementById('playVid').onclick = function () {
+  document.getElementById('vid').play();
+};
+function pauseVid() {
+  document.getElementById('vid').pause();
+}
+$('.owl-members').owlCarousel({
+  loop: true,
+  margin: 20,
+  nav: true,
+  navText: [
+    '<i class="fa-solid fa-arrow-left"></i>',
+    '<i class="fa-solid fa-arrow-right"></i>'
+  ],
+  dots: false,
+  center: true,
+  autoplay: true,
+  items: 2
+});
+$('.owl-slider').owlCarousel({
+  loop: true,
+  margin: 20,
+  nav: true,
+  navText: [
+    '<i class="fa-solid fa-arrow-left"></i>',
+    '<i class="fa-solid fa-arrow-right"></i>'
+  ],
+  dots: false,
+  center: true,
+  autoplay: false,
+  items: 1
+});
+const move = document.querySelector('.box');
+const moveTwo = document.querySelector('.boxTwo');
+window.addEventListener('scroll', () => {
+  const scrolled = window.scrollY;
+  const val = scrolled * 0.2;
+  move.style.transform = `translateX(${-0.05 * val}%)`;
+  moveTwo.style.transform = `translateX(${0.05 * val}%)`;
+});
+var myVid = document.querySelector('.video-thumb');
+window.addEventListener('scroll', () => {
+  var scrolled = window.scrollY - myVid.clientHeight;
+  var val = scrolled * 0.25;
+  myVid.style.transform = `scale(${-0.09 * val}%)`;
+  myVid.style.borderRadius = `${0.06 * val}%`;
+});
 
