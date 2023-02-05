d1e019f02b9e6afc09fad1b533e9c6011509fe5e
universe1216 <marcus727.dev@outlook.com>
2023-02-05 23:53:37 +0530

    Adding EMS Dashboard

---
+++ ems/assets/js/main.js
@@ -0,0 +1,57 @@
+$(function () {
+  "use strict";
+
+  //===== Counter
+
+  $(".counter").counterUp({
+    delay: 10,
+    time: 2000,
+  });
+
+  // read more btn toggle
+
+  $(document).ready(function () {
+    $(".read-more").click(function () {
+      $(this).text($(this).text() == "Read More" ? "Read Less" : "Read More");
+    });
+  });
+});
+
+// Go to Top
+
+// scroll to top
+
+//Get the button
+var mybutton = document.getElementById("topBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (
+    document.body.scrollTop > 200 ||
+    document.documentElement.scrollTop > 200
+  ) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
+// Read More Read Less
+
+var dayselect = document.getElementById("dayselect");
+var selectedDay = document.querySelectorAll(".selectedday");
+for (var i = 0; i < selectedDay.length; i++)
+  selectedDay[i].addEventListener("click", function handleClick() {
+    var text = this.getAttribute("title");
+    dayselect.innerHTML = text;
+  });
